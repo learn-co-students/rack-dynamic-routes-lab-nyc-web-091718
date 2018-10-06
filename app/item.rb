fc49b84c91af21a97af526137e5b12a9f0f7821c
TurtleFeeder <t.choong@yahoo.com>
2018-10-06 18:05:45 -0400

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,22 @@
 class Item
   attr_accessor :name, :price
+  @@items = []
+
+  def self.all
+    @@items
+  end
+
+  def self.item_names
+    self.all.map {|item| item.name}
+  end #end item_names helper class method
+
+  def self.find_by_name(name)
+    self.all.find {|item| item.name == name}
+  end #end find_by_name helper class method
 
   def initialize(name,price)
     @name = name
     @price = price
+    self.class.all << self
   end
 end
