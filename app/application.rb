7ef96738a770e8bf1cd47cf8481e447311b3d05a
eric-klimowich <eklimowich@hotmail.com>
2018-10-08 08:27:05 -0400

    Items and status

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_req = req.path.split("/items/").last
+      assign_item_req = @@items.find{|i| i.name == item_req}
+      if assign_item_req
+        resp.write "#{assign_item_req.price}\n"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
